776114c3868794c9ece053c39727ca3e540c7e98
Shota Takayama <takayama@shanon.co.jp>
2021-04-13 01:57:08 +0900

    support ros-apartment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_154859) do
+ActiveRecord::Schema.define(version: 2021_04_12_164334) do
+
+  create_table "tenants", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "host"
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "name"
